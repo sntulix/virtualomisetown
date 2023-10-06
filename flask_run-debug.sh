b3fa16dc3718214ac3de5a6c3faf523bb8acdf91
Takahiro Shizuki <takahiro_shizuki@yahoo.ne.jp>
2023-10-06 19:49:05 +0900

    fix: run shells.

--- flask_run-debug.sh
+++ flask_run-debug.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export FLASK_RUN_HOST=localhost
-export FLASK_RUN_PORT=5001
+export FLASK_RUN_HOST=shoutengai.futuregadget.com
+export FLASK_RUN_PORT=80
 
 flask run -h $FLASK_RUN_HOST -p $FLASK_RUN_PORT \
 #--cert server.crt --key server-nopassword.key \
